364e3e139164fd0f991990541f9b642ff717cb1c
sdowns <sdowns1017@gmail.com>
2026-01-30 17:42:01 -0500

    Add refined verifier output, added stability check logic, TODO: Add comments and readme for readability and code understanding

--- src/Verifier.py
+++ src/Verifier.py
@@ -1,4 +1,4 @@
-
+# SHANE DOWNS AND SHASHANK GUTTA VERIFIER
 
 def read_matching_output(output_filename):
     matches = {}
@@ -9,7 +9,7 @@ def read_matching_output(output_filename):
                 matches[int(hospital_id)] = int(student_id.strip())
     except IOError:
         print("Error reading output file from Matching Engine")
-    print(matches)
+    #print(matches)
     return matches
 
 def read_input(input_filename):
@@ -45,18 +45,31 @@ def check_validity(n, matches):
     elif set(matches.values()) != set(range(1, n + 1)):
         return False, "Invalid student ids"
     else:
-        return True, "Valid Matching"
+        return True, "Valid Matchings"
 
-def check_stability(matches):
-    pass
+def check_stability(n, matches, hospital_prefs, student_prefs):
+    student_to_hospital = {s: h for h, s in matches.items()}
+    for h in range(1, n + 1):
+        curr_student = matches[h]
+        for s in hospital_prefs[h]:
+            if s == curr_student:
+                break
+            students_curr_hospital = student_to_hospital[s]
+            student_pref_list = student_prefs[s]
+            if student_pref_list.index(h) < student_pref_list.index(students_curr_hospital):
+                return False, f"Unstable: Hospital {h} and student {s} form blocking pair"
 
+    return True, "Stable Matchings"
 
 if __name__ == '__main__':
-    read_matching_output("../output.out")
-    print("---------")
-
+    matches = read_matching_output("../output.out")
     n, hospital_prefs, student_prefs = read_input("../input/basic_stable_input.txt")
-    print(n)
-    print(hospital_prefs)
-    print(student_prefs)
 
+    is_valid, message = check_validity(n, matches)
+    print(message)
+
+    if is_valid:
+        is_stable, message = check_stability(n, matches, hospital_prefs, student_prefs)
+        print(message)
+    else:
+        print("INVALID")
